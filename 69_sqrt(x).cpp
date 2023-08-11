14f666d5a88f42b65aa5a3b92827a7b08c8c385b
Hitesh Nagar <74867491+hiteshnagar2611@users.noreply.github.com>
2023-08-11 11:50:06 +0530

    Update 69_sqrt(x).cpp

--- 69_sqrt(x).cpp
+++ 69_sqrt(x).cpp
@@ -1,3 +1,4 @@
+// solution 1
 class Solution {
 public:
     int mySqrt(int x) {
@@ -6,5 +7,26 @@ public:
             y++;
         }
         return y-1;
+    }
+};
+
+
+// solution 2
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        
+        double half = myPow(x, n / 2);
+        
+        if (n % 2 == 0) {
+            return half * half;
+        } else if (n > 0) {
+            return x * half * half;
+        } else {
+            return (1 / x) * half * half;
+        }
     }
 };
