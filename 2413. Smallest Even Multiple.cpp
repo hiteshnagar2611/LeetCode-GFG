d9eb30af925bf1ef38998a3ab7569b576039d681
Hitesh Nagar <74867491+hiteshnagar2611@users.noreply.github.com>
2023-09-15 20:48:10 +0530

    Create 2413. Smallest Even Multiple.cpp

---
+++ 2413. Smallest Even Multiple.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+       for(int i=n;;i++){
+           if(i%2==0 && i%n==0){
+               return i;
+               break;
+           }
+       } 
+    }
+};
